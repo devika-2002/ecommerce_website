66a4a6245539970e6181ff802325d64452a439d8
devika kumari <devika21@navgurukul.org>
2023-11-30 18:37:31 +0530

    add card

---
+++ src/Components/Card3/Card3.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import "../Card/Card.css"
+
+const Card3= () => {
+  return (
+    <>
+        <div className="container-fluid px-5">
+          <h2 className="mt-5">Fashion Top Deals</h2>
+          <div className="scrolling-wrapper row flex-row flex-nowrap">
+            
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+            <div className="col-3">
+              <div className="card card-block"></div>
+            </div>
+          </div>
+      </div>
+    </>
+  );
+};
+
+export default Card3;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Carousel from "./Components/Carousel"
-const page=()=>{
+import React from 'react';
+import Navbar from '@/Components/Navbar';
+import Carousel from '@/Components/Carousel';
+import Card from '@/Components/Card/Card';
+import Card1 from '@/Components/Card1/Card1';
+import Card2 from '@/Components/Card2/Card2';
+import Card3 from '@/Components/Card3/Card3';
+
+const Page = () => {
   return (
     <div>
-      <Navbar/>
-      <Carousel/>
+      <Navbar />
+      <Carousel />
+      <Card/>
+      <Card1/>
+      <Card2/>
+      <Card3/>
     </div>
-  )
-}
+  );
+};
 
-export default page;
+export default Page;
